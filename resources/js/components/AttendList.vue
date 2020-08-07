848cb0eda3abcb8499302fb4e508449c8d2b0dc0
zwe man soe <zwemansoe@management-partners.co.jp>
2020-08-07 13:29:09 +0630

    first-commit

--- resources/js/components/AttendList.vue
+++ resources/js/components/AttendList.vue
@@ -7,8 +7,8 @@
                         <option value="" disabled selected>Please select employee</option>        
                       
                         <option v-bind:key="emp.id" v-for="emp in emps"  >{{ emp.id }} {{emp.name }}</option>
-                        
-                    </select>
+                     
+                    </select>                      
                  </div>  
                 <div class="col-md-4 offset-md-2"> 
                     <select class="form-control" id="selectDate" name="date_selected" required focus v-model="select_date">
@@ -16,10 +16,72 @@
                       
                         <option v-bind:key="date.id" v-for="date in dates"  >{{ date.recordedDateTime }}</option>
                         
-                    </select>
+                    </select>                       
                  </div>  
-                </div>                  
-            </div>          
+                </div> 
+            </div>   
+            
+            <div class="container mt-5">
+                <!-- //form -->
+                    <div class="row">
+                        <div class="col-md-4">
+                              <button type="button" class="btn btn-primary">Primary1</button>
+                        </div>
+                         <div class="col-md-4 offset-md-2">
+                            <button type="button" class="btn btn-primary">Primary2</button>
+                         </div> 
+                    </div>                 
+                 
+                    <div class="row mt-3">
+                          <div class="col-md-4"></div>
+                          <div class="col-md-4 offset-md-2">
+                              <button type="button" class="btn btn-primary pull-right">Primary3</button>
+                          </div>
+                    </div>
+
+                      <div class="row mt-5">
+                            <div class="col-md-4"> {{this.select_date}}</div>                          
+                      </div>  
+                       <div class="row">
+                            <div class="col-md-4"> Employee No.{{emp_no}}</div>                          
+                      </div>    
+                      <div class="row">
+                            <div class="col-md-4"> Name: {{emp_name}}</div>                          
+                      </div>  
+                    <table class="table table-bordered">
+                        <thead>
+                            <tr>
+                            <th scope="col"></th>
+                            <th scope="col">AM</th>
+                            <th scope="col">PM</th>
+                            <th scope="col">Total Hours</th>
+                            <th scope="col"></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                            <th scope="row">1</th>
+                            <td>Mark</td>
+                            <td>Otto</td>
+                            <td>@mdo</td>
+                            </tr>
+                            <tr>
+                            <th scope="row">2</th>
+                            <td>Jacob</td>
+                            <td>Thornton</td>
+                            <td>@fat</td>
+                            </tr>
+                            <tr>
+                            <th scope="row">3</th>
+                            <td>Larry</td>
+                            <td>the Bird</td>
+                            <td>@twitter</td>
+                            </tr>
+                         </tbody>
+                    </table>
+
+            </div>
+       
     </div>    
 </template>
 
@@ -28,9 +90,11 @@
         data() {
             return {
                 select_employee:'',
-                select_date:'',
+                select_date:'',          
                 emps:[],
-                dates:[]
+                dates:[],
+                emp_no:'',
+                emp_name:''
             }
         },
         created() {
@@ -39,6 +103,7 @@
                 .then(response => {
                     // console.log(response);
                     this.emps=response.data;
+
                     // console.log(this.emps);
                 });
 
@@ -50,12 +115,30 @@
                 // console.log(this.dates);
             });
         },
+        watch: {
+           select_employee:function (val) {
+               if(val!='' && this.select_date!=''){
+                   this.attendForm();
+               }
+               let split_name=val.split(" ");
+               this.emp_no=split_name[0];
+               this.emp_name=split_name[1]+" "+split_name[2];
+            },
+            select_date:function (val) {
+                if(val!='' && this.select_employee!=''){
+                     this.attendForm();
+                }
+            },           
+        },
         methods: {
-            deleteBook(id) {
+            delete(id) {
                 this.axios
                     .delete(``)
                     .then(response => {                       
                     });
+            },
+            attendForm(){
+                
             }
         }
     }
